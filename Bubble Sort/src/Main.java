5b202cd71275c41b4c4d42943764234ac7fa5427
SRIDHARSHAN K M D <87009056+Sridharshan18@users.noreply.github.com>
2024-06-24 12:02:35 +0530

    Update Main.java

--- Bubble Sort/src/Main.java
+++ Bubble Sort/src/Main.java
@@ -29,10 +29,10 @@ public class Main {
 
     public static void bubbleSort(int[] arr , int n)
     {
-        for(int i = n-1; i>=0; i--)
+        for(int i = 0; i<n-1; i++)
         {
             int didSwap = 0;
-            for(int j = 0 ; j<= i-1; j++)
+            for(int j = 0 ; j<= n-i-1; j++)
             {
                 if(arr[j]>arr[j+1])
                 {
